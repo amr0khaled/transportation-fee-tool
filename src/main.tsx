db7f68716523c6203a19d848acebc8bd41b875ea
Amr Khaled <redwolf4325@gmail.com>
2025-10-04 05:34:49 +0300

    Added a manifest + favicon

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import IndexPage from './pages/IndexPage.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.body!).render(
   <StrictMode>
     <IndexPage />
   </StrictMode>,
