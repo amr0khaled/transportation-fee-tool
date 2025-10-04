a0a592bdd78da3154d0708d7a5c82d4348064d6c
Amr Khaled <redwolf4325@gmail.com>
2025-10-05 01:44:38 +0300

    update

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -20,7 +20,10 @@ export default function Header() {
   }, [theme])
   return (
     <header className='header'>
-      <span className='brand'>Ograt Mashro3</span>
+      <div className='flex gap-2 sm:gap-4 items-center'>
+        <img className='size-12' alt='' src='/favicon.svg' />
+        <span className='brand'>Ograt Mashro3</span>
+      </div>
       <ul className='list-buttons'>
         <Tooltip>
           <TooltipTrigger asChild>
